3376d08709411b3fc142b92d2dda609f90abac9f
glivermore2025 <griffenslivermore@gmail.com>
2025-06-11 17:23:41 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp;
