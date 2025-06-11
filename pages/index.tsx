17c732f6ddf833d8e90713fb45b9b63e20022fe2
glivermore2025 <griffenslivermore@gmail.com>
2025-06-10 21:55:25 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import WaitlistForm from '/components/WaitlistForm';
+import WaitlistForm from '@/components/WaitlistForm';
 
 export default function Home() {
   return (
