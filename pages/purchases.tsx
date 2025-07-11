f80752df0b9da3e4af3300893dc180b988cc01bf
glivermore2025 <griffenslivermore@gmail.com>
2025-07-10 19:34:19 -0500

    Update purchases.tsx

--- pages/purchases.tsx
+++ pages/purchases.tsx
@@ -41,6 +41,8 @@ const { data, error } = await supabase
   .eq('user_id', user.id);
 
     console.log('Logged in user ID:', user.id);
+console.log('DATA:', data);
+console.log('ERROR:', error);
 
 if (error) {
   console.error('Error fetching purchases:', error);
