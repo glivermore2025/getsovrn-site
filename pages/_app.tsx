fa30482e542bb650d6470f2d52af6fa434057af8
glivermore2025 <griffenslivermore@gmail.com>
2025-07-13 15:24:10 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../lib/authContext';
-import Layout from '../components/layout'; // If using a layout wrapper
+import Layout from '../components/Layout'; // If using a layout wrapper
 import '../styles/globals.css'; // If you have global styles
 
 function MyApp({ Component, pageProps }: AppProps) {
